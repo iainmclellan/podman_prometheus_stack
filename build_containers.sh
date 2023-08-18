be18ab113eebcf9105024a16591c629ef455697c
Iain Mclellan <iain.mclellan@me.com>
2023-08-18 16:23:19 +0100

    fix: all fixes applied

--- build_containers.sh
+++ build_containers.sh
@@ -18,6 +18,12 @@ buildah bud -t traefik-container ./container_setup/traefik
 # # Build Kuma Uptime Container
 buildah bud -t kuma-uptime-container ./container_setup/kuma-uptime
 
+# Build Node Exporter Container
+buildah bud -t node-exporter-container ./container_setup/node-exporter
+
+# Build cAdvisor Container
+buildah bud -t cadvisor-container ./container_setup/cadvisor
+
 # Run Traefik Container
 podman run -d --name=traefik --network=podman -p 8080:80 traefik-container
 
@@ -40,3 +46,11 @@ podman run -d --name=grafana --network=podman -p 3000:3000 \
 podman run -d --name=kuma-uptime --network=podman -p 3001:3001 \
   -l "traefik.http.routers.kuma-uptime.rule=Host(`kuma-uptime.example.com`)" \
   kuma-uptime-container
+
+# Run Node Exporter Container
+podman run -d --name=node-exporter --network=prometheus_network -p 9100:9100 \
+  node-exporter-container
+
+# Run cAdvisor Container
+podman run -d --name=cadvisor --network=prometheus_network -p 8081:8081 \
+  cadvisor-container
